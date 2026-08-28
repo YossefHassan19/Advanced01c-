36ff5ecfb2e8e125944a13dcd5a7a60ca2008042
YossefHassan19 <yossef.hassan678@gmail.com>
2026-08-29 00:35:19 +0300

    Question01

--- Program.cs
+++ Program.cs
@@ -4,6 +4,15 @@
     {
         static void Main(string[] args)
         {
+            #region Question01
+
+            //What is a generic class? Why use generics?
+
+            //A generic class allows you to write a class with type parameters
+            //1-Type safe                      2-Code reuseability
+            //3-Better performance             4-Cleaner code
+
+            #endregion
 
         }
     }
